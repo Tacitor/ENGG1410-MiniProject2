fc6522e85eda6630fa62ea053e022d459cef616f
Tacitor <lukasjkrampitz@gmail.com>
2022-11-25 10:27:38 -0500

    Get the our_user data from the command line

--- gps.c
+++ gps.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define NAME_LEN 100
 
@@ -23,7 +24,8 @@ typedef struct distToUser
     double dist;
 } distToUser_t;
 
-user_t scan_user(double, double, double, double, char[]);
+user_t scan_user(double, double, double, double, char *);
+void getOur_user(user_t *);
 
 int main(void)
 {
@@ -33,12 +35,42 @@ int main(void)
 
     printf("Welcome to the GPS position calculator\n");
 
+    // get the data for our_user
+    getOur_user(&our_user);
+
+    printf("Second Hi %s! Your position is (%lf, %lf, %lf) at %lf nanoseconds.\n", our_user.name, our_user.latitude, our_user.longitude, our_user.altitude, our_user.time);
+
     return 0;
 }
 
-user_t scan_user(double lat, double lon, double alt, double time, char name[NAME_LEN])
+/**
+ * Prompt the user for input from the cli about their position, name, and time.
+ */
+void getOur_user(user_t *our_user)
 {
-    user_t returnUser = {lat, lon, alt, time, name}; // create the
+    double lat, lon, alt, time;
+    char name[NAME_LEN];
+
+    printf("\nPlease enter your name: ");
+    scanf("%s", &name);
+
+    printf("Please enter your position (latitude, longitude, altitude): ");
+    scanf("%lf, %lf, %lf", &lat, &lon, &alt);
+
+    printf("Please enter your time in nano seconds: ");
+    scanf("%lf", &time);
+
+    // debug the user input
+    printf("First Hi %s! Your position is (%lf, %lf, %lf) at %lf nanoseconds.\n", name, lat, lon, alt, time);
+
+    // fill the struct
+    *our_user = scan_user(lat, lon, alt, time, name);
+}
+
+user_t scan_user(double lat, double lon, double alt, double time, char *name)
+{
+    user_t returnUser = {lat, lon, alt, time, "\0"}; // create the struct
+    strcpy(returnUser.name, name);
 
     return returnUser;
 }

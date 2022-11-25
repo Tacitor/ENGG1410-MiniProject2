332facb571525672d35cf41934ffe24b7c5c7f4d
Tacitor <lukasjkrampitz@gmail.com>
2022-11-25 09:11:10 -0500

    Add in the structs needed

--- gps.c
+++ gps.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 
+#define NAME_LEN 100
+
+/**
+ * A Struct that hold almost everything needed for traking a user.
+ */
+typedef struct user
+{
+    double latitude;
+    double longitude;
+    double altitude;
+    double time;
+    char name[NAME_LEN];
+} user_t;
+
+/**
+ * A Struct that holds the name of another user. And the distance to the main user (our_user)
+ */
+typedef struct distToUser
+{
+    char name[NAME_LEN];
+    double dist;
+} distToUser_t;
+
+user_t scan_user(double, double, double, double, char[]);
+
 int main(void)
 {
 
-    printf("This will be a GPS coordinates program\n");
+    user_t our_user;
+    user_t other_users[2];
+
+    printf("Welcome to the GPS position calculator\n");
 
     return 0;
+}
+
+user_t scan_user(double lat, double lon, double alt, double time, char name[NAME_LEN])
+{
+    user_t returnUser = {lat, lon, alt, time, name}; // create the
+
+    return returnUser;
 }
